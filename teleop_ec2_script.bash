fa5c972ead7cebcd966520a028678d6f243872d6
Adrian Lendinez <adrian.lendinez@outlook.com>
2024-06-19 10:25:02 +0200

    Add files via upload

---
+++ teleop_ec2_script.bash
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+export USE_MIDDLEWARE=true
+eed9980d2c0a8a.elb.eu-west-2.amazonaws.com
+#export MIDDLEWARE_ADDRESS=crop.5gera.net
+export MIDDLEWARE_ADDRESS=ec2-18-101-149-37.eu-south-2.compute.amazonaws.com:31000
+export MIDDLEWARE_USER=ad20f254-dc3b-406d-9f15-b73ccd47e867
+export MIDDLEWARE_PASSWORD=middleware
+#signal
+export MIDDLEWARE_TASK_ID=c0e612c4-78e5-46db-87f8-3ac99bf64b18
+#reference
+#export MIDDLEWARE_TASK_ID=6d7b20ad-8459-4f00-91f4-816dded6395c
+export MIDDLEWARE_ROBOT_ID=300c719a-1c06-4500-a13a-c2e20592b273
+# TOPICS TO SEND FROM ROBOT TO NETWORK APPLICATION
+export TOPICS_FROM_SERVER='[{"name": "/robot/robotnik_base_control/cmd_vel_unstamped", "type": "geometry_msgs/msg/Twist"}]'
+# TOPICS TO SEND FROM NETWORK APPLICATION BACK TO ROBOT
+export TOPICS_TO_SERVER='[{"name": "/robot/robotnik_base_control/odom", "type": "nav_msgs/msg/Odometry"},{"name": "/robot/front_rgbd_camera/rgb/image_raw/compressed", "type": "sensor_msgs/msg/CompressedImage"}]'
+# RUN RELAY CLIENT
+python3 -m era_5g_relay_network_application.client
